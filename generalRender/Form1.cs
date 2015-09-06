ec660468ae7a861b086bb5bea1301076161f2e4d
NickJ1984 <nikolay.yudaev@gmail.com>
2015-09-06 23:52:45 +0300

    first

---
+++ generalRender/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generalRender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            frame box = new frame(100, 100);
+            box.graphic.DrawRectangle(Pens.Aqua, 0, 0, 99, 99);
+            e.Graphics.DrawImage(box.bmap, new Point(100, 100));
+        }
+    }
+}
---
+++ generalRender/genRender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace generalRender
+{
+    class frame
+    {
+        public Graphics graphic { get; private set; }
+        public Bitmap bmap { get; private set; }
+
+        public frame(int width, int height)
+        {
+            bmap = new Bitmap(width, height);
+            graphic = Graphics.FromImage(bmap);
+        }
+
+        public void reset(int width, int height)
+        {
+            bmap = new Bitmap(width, height);
+            graphic = Graphics.FromImage(bmap);
+        }
+
+        public void reset()
+        {
+            reset(bmap.Width, bmap.Height);
+        }
+
+    }
+    class genRender
+    {
+    }
+}
