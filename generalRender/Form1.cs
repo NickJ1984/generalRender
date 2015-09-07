fe87a04e0844d75eb27de3c2b98b8a5951a546f2
NickJ1984 <nikolay.yudaev@gmail.com>
2015-09-07 16:30:28 +0300

    directions & neighbours

--- generalRender/Form1.cs
+++ generalRender/Form1.cs
@@ -22,6 +22,9 @@ namespace generalRender
             frame box = new frame(100, 100);
             box.graphic.DrawRectangle(Pens.Aqua, 0, 0, 99, 99);
             e.Graphics.DrawImage(box.bmap, new Point(100, 100));
+            mazeElement mElem = new mazeElement(50, 50, Color.Green, e_direction.up | e_direction.down | e_direction.left | e_direction.right);
+            e.Graphics.DrawImage(mElem.picture, new Point(100, 200));
+
         }
     }
 }
--- generalRender/genRender.cs
+++ generalRender/genRender.cs
@@ -11,6 +11,7 @@ namespace generalRender
     [Flags]
     public enum e_direction
     {
+        none = 0,
         left = 1,
         right = 2,
         up = 4,
@@ -18,6 +19,8 @@ namespace generalRender
     };
     partial class mazeElement
     {
+        public delegate void changeBoundaries(e_direction direction);
+        public event changeBoundaries onChangeBoundaries;
         public Bitmap picture
         {
             get
@@ -25,6 +28,7 @@ namespace generalRender
                 return box.bmap;
             }
         }
+
         public int width { get; private set; }
         public int height { get; private set; }
         public Color color { get; private set; }
@@ -36,19 +40,30 @@ namespace generalRender
         {
             width = _width;
             height = _height;
+            box = new frame(width, height);
             setColor(_color);
             setDirection(_direction);
         }
 
+        public mazeElement(mazeElement element)
+        {
+            width = element.width;
+            height = element.height;
+            setColor(element.color);
+            setDirection(element.direction);
+        }
+    }
+
+    partial class mazeElement
+    {
         public void setColor(Color _color)
         {
             cPen = new Pen(_color);
             color = _color;
         }
-
         public void setDirection(e_direction _direction)
         {
-            if (direction == _direction) return;
+            if (direction == _direction || _direction == 0) return;
             direction = _direction;
             box.reset();
             Pen cPen = new Pen(color);
@@ -57,12 +72,40 @@ namespace generalRender
             if ((direction & e_direction.up) == e_direction.up) setBound_up();
             if ((direction & e_direction.left) == e_direction.left) setBound_left();
             if ((direction & e_direction.right) == e_direction.right) setBound_right();
+
+            if (onChangeBoundaries != null) onChangeBoundaries(direction);
         }
-
-        
-
+        public void addDirection(e_direction _direction)
+        {
+            setDirection(direction | _direction);
+        }
+        public void setNeighboursBounds(mazeElement left, mazeElement right, mazeElement up, mazeElement down)
+        {
+            e_direction temp = new e_direction();
+            if ((left.direction & e_direction.right) == e_direction.right) temp = temp | e_direction.left;
+            if ((right.direction & e_direction.left) == e_direction.left) temp = temp | e_direction.right;
+            if ((up.direction & e_direction.down) == e_direction.down) temp = temp | e_direction.up;
+            if ((down.direction & e_direction.up) == e_direction.up) temp = temp | e_direction.down;
+            if(temp > 0) addDirection(temp);
+        }
+        public e_direction getNeighbourBound(e_direction _direction)
+        {
+            if ((direction & _direction) != _direction) return e_direction.none;
+            switch(direction)
+            {
+                case e_direction.down:
+                    return e_direction.up;
+                case e_direction.up:
+                    return e_direction.down;
+                case e_direction.left:
+                    return e_direction.right;
+                case e_direction.right:
+                    return e_direction.left;
+                default:
+                    return e_direction.none;
+            }
+        }
     }
-
     partial class mazeElement
     {
         private void setBound_down()
